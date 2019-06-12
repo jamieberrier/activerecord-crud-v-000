d2c4bab6b6a1f0fe68cac392d00535aaafd67f36
jamieberrier <berrier.jamie@gmail.com>
2019-06-12 18:01:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,9 +29,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    title  = :title,
-    release_date = :release_date,
-    director: "George Roy Hill",
+    title  = :title
+    release_date = :release_date
+    director = :director
     lead: "Paul Newman",
     in_theaters: false
   end
