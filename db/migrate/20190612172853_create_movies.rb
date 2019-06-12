81eae2c460578a81525f9cefba8841d6cd9b60bd
jamieberrier <berrier.jamie@gmail.com>
2019-06-12 17:30:51 +0000

    Automatically backed up by Learn

--- db/migrate/20190612172853_create_movies.rb
+++ db/migrate/20190612172853_create_movies.rb
@@ -1,5 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     add_column :movies, :title, :string
+    add_column :movies, :release_date, :integer
+    add_column :movies, :director, :string
   end
 end
